655c7ed117838c631013fbbe3d8bf0fa972c90ac
Kaspars.Burvis <kaspars.burvis@eleks.com>
2022-06-08 17:22:44 +0300

    feature/button-replace  * deleted unused components

--- src/components/set-game-status-btn/set-game-status-btn.jsx
+++
@@ -1,7 +0,0 @@
-import './set-game-status-btn.scss';
-
-function SetGameStatusBtn({ children }) {
-  return <button className="set-game-status-btn">{children}</button>;
-}
-
-export default SetGameStatusBtn;
