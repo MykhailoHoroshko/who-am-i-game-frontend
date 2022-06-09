b7ae9e03bce95acd88ca35c473c801df1c529355
Kaspars.Burvis <kaspars.burvis@eleks.com>
2022-06-09 20:03:09 +0300

    feature/button-replace  * refactoring loading screen

--- src/screens/loading/loading.jsx
+++ src/screens/loading/loading.jsx
@@ -1,26 +1,40 @@
 import GameTitle from '../../components/game-title/game-title';
-import LeaveGameBtn from '../../components/leave-game-btn/leave-game-btn';
 import Timer from '../../components/timer/timer';
 import GuessCharacterModal from '../../components/modals/guess-a-character/guess-a-character';
 import './loading.scss';
 import { useState } from 'react';
+import Btn from '../../components/btn/btn';
+import LeaveGameModal from '../../components/modals/leave-game/leave-game';
 
 function Loading() {
   const [displayModal, setDisplayModal] = useState(true);
+  const [modalActive, setModalActive] = useState(false);
+
+  const leaveGameModalHandle = () => setModalActive(true);
 
   return (
     <div className="loading">
-      <GameTitle />
-      <h3 className="loading__queue-number">23 PLAYERS ARE IN QUEUE</h3>
-      <h3 className="loading__waiting-message">
-        PLEASE WAIT UNTIL WE FIND YOUR OPPONENTS
-      </h3>
-      <Timer />
-      <LeaveGameBtn />
-      <GuessCharacterModal
-        displayModal={displayModal}
-        setDisplayModal={setDisplayModal}
-      />
+      <div className="loading__wrapper">
+        <GameTitle />
+        <h3 className="loading__queue-number">23 PLAYERS ARE IN QUEUE</h3>
+        <h3 className="loading__waiting-message">
+          PLEASE WAIT UNTIL WE FIND YOUR OPPONENTS
+        </h3>
+        <Timer />
+        <div className="loading__btn-wrap">
+          <Btn onClick={leaveGameModalHandle} className="btn-pink-solid">
+            Leave Game
+          </Btn>
+        </div>
+        <LeaveGameModal
+          showModal={modalActive}
+          setModalActive={setModalActive}
+        />
+        <GuessCharacterModal
+          displayModal={displayModal}
+          setDisplayModal={setDisplayModal}
+        />
+      </div>
     </div>
   );
 }
