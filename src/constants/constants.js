5df40b6f5333129ba2c743053f671919b912787f
MinoUni <maks.reva.ua@gmail.com>
2022-07-25 12:58:04 +0300

    fix consts

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 const WAITING_FOR_PLAYERS =
-  'com.eleks.academy.whoami.core.state.WaitingForPlayers';
+  'com.eleks.academy.whoami.core.state.impl.WaitingForPlayers';
 const SUGGESTING_CHARACTERS =
-  'com.eleks.academy.whoami.core.state.SuggestingCharacters';
+  'com.eleks.academy.whoami.core.state.impl.SuggestingCharacters';
 const PROCESSING_QUESTION =
-  'com.eleks.academy.whoami.core.state.ProcessingQuestion';
+  'com.eleks.academy.whoami.core.state.impl.ProcessingQuestion';
 const NUMBER_OF_PLAYERS = 4;
 const READY = 'READY';
 const NOT_READY = 'NOT_READY';
